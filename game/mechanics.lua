391c9cc3334d564176f6b58a44da89c46150e170
Sebastian Jensen <snj@3dintegrated.com>
2018-09-14 19:57:13 +0200

    Added ability to unlock abilities

--- game/mechanics.lua
+++ game/mechanics.lua
@@ -11,4 +11,14 @@ function mechanics.add_stat(a)
     return mechanics.map_stat(function(v) return a + v end)
 end
 
+function mechanics.unlock_ability(...)
+    local ability = require "ability"
+    local l1 = list(...)
+        :map(function(p) return ability(p) end)
+    return function(l2)
+        l2 = l2 or list()
+        return l2:concat(l1)
+    end
+end
+
 return mechanics
